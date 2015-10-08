ae85581d02c5b9fd274dc667f4c4c8d0d98b64a2
viki <viki@c3po.(none)>
2015-10-08 13:24:39 -0700

    Fixed a bug in the node code.

--- fastsim_to_turtlebot/fastsim_to_turtlebot.py
+++ fastsim_to_turtlebot/fastsim_to_turtlebot.py
@@ -4,54 +4,66 @@
 
 import rospy
 from geometry_msgs.msg import Twist
+from kobuki_msgs.msg import BumperEvent
+from std_msgs.msg import Bool
+from std_msgs.msg import Float32
 
 class TranslateFastsimTurtlebot():
     def __init__(self):
         # initiliaze
         self.speed_left=0
         self.speed_right=0
+        self.left_bumper=False
+        self.right_bumper=False
 
         rospy.init_node('TranslateFastsimTurtlebot', anonymous=False)
 
         # tell user how to stop TurtleBot
         rospy.loginfo("To stop TurtleBot CTRL + C")
 
-        # What function to call when you ctrl + c    
-        rospy.on_shutdown(self.shutdown)
         
-        # Create a publisher which can "talk" to TurtleBot and tell it to move
-        # Tip: You may need to change cmd_vel_mux/input/navi to /cmd_vel if you're not using TurtleBot2
-        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
-     
+        # Get speed sent to fastsim and translate it to turtlebot
         self.sl_fastsim = rospy.Subscriber("/simu_fastsim/speed_left", Float32, self.Speed_left_callback)
         self.sr_fastsim = rospy.Subscriber("/simu_fastsim/speed_right", Float32, self.Speed_right_callback)
+        self.cmd_vel = rospy.Publisher('cmd_vel_mux/input/navi', Twist, queue_size=10)
 
+     
+        # Get bumpers from the turtlebot and translate it to fastsim scripts
+
+        self.sb = rospy.Subscriber("/mobile_base/evts/bumper", BumperEvent, self.Bumper_callback)
+
+        self.left_bumper_fastsim = rospy.Publisher('/simu_fastsim/left_bumper', Bool, queue_size=10)
+        self.right_bumper_fastsim = rospy.Publisher('/simu_fastsim/right_bumper', Bool, queue_size=10)
+
+ 
+        # What function to call when you ctrl + c    
+        rospy.on_shutdown(self.shutdown)
 
 
         #TurtleBot will stop if we don't keep telling it to move. 
-        r = rospy.Rate(20);
+        r = rospy.Rate(10);
 
         # Twist is a datatype for velocity
         move_cmd = Twist()
 
+
+
         # as long as you haven't ctrl + c keeping doing...
         while not rospy.is_shutdown():
-
-            # let's go forward at 0.2 m/s
+            #rospy.loginfo("I am in the loop !")
+            
             move_cmd.linear.x = (self.speed_left+self.speed_right)/2.0
-            # let's turn at 0 radians/s
             move_cmd.angular.z = (self.speed_right-self.speed_left)/0.5
 
             # publish the velocity
             self.cmd_vel.publish(move_cmd)
-            # wait for 0.1 seconds (10 HZ) and publish again
-            r.sleep()
-                        
-    def Speed_left_callback(self,data):
-        self.speed_left=data
 
-    def Speed_right_callback(self,data):
-        self.speed_right=data
+            # publish the bumpers
+            self.left_bumper_fastsim.publish(self.left_bumper)
+            self.right_bumper_fastsim.publish(self.right_bumper)
+ 
+            # wait 
+            r.sleep()
 
     def shutdown(self):
         # stop turtlebot
@@ -60,6 +72,25 @@ class TranslateFastsimTurtlebot():
         self.cmd_vel.publish(Twist())
   # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
+                        
+    def Speed_left_callback(self,data):
+        self.speed_left=float(data.data)
+        rospy.loginfo("Received speed left: "+str(self.speed_left))
+
+    def Speed_right_callback(self,data):
+        self.speed_right=float(data.data)
+        rospy.loginfo("Received speed right: "+str(self.speed_right))
+
+    def Bumper_callback(self,data):
+        rospy.loginfo("Received bumper")
+        if (data.bumper==data.LEFT):
+            self.left_bumper=data.state==data.PRESSED
+        elif (data.bumper==data.RIGHT):
+            self.right_bumper=data.state==data.PRESSED
+
+
+
+
 if __name__ == '__main__':
     try:
         TranslateFastsimTurtlebot()
