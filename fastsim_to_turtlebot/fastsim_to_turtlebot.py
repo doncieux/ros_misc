38795536e944d2f324c2334109a9b7dd6a627ae8
doncieux <stephane.doncieux@upmc.fr>
2015-11-01 20:16:42 +0100

    Changed topic names

--- fastsim_to_turtlebot/fastsim_to_turtlebot.py
+++ fastsim_to_turtlebot/fastsim_to_turtlebot.py
@@ -4,9 +4,12 @@
 
 import rospy
 from geometry_msgs.msg import Twist
-from kobuki_msgs.msg import BumperEvent
+from kobuki_msgs.msg import SensorState
 from std_msgs.msg import Bool
 from std_msgs.msg import Float32
+from sensor_msgs.msg import LaserScan
+
+speed_max=0.2
 
 class TranslateFastsimTurtlebot():
     def __init__(self):
@@ -15,6 +18,7 @@ class TranslateFastsimTurtlebot():
         self.speed_right=0
         self.left_bumper=False
         self.right_bumper=False
+        self.lasers=LaserScan()
 
         rospy.init_node('TranslateFastsimTurtlebot', anonymous=False)
 
@@ -30,10 +34,13 @@ class TranslateFastsimTurtlebot():
      
         # Get bumpers from the turtlebot and translate it to fastsim scripts
 
-        self.sb = rospy.Subscriber("/mobile_base/evts/bumper", BumperEvent, self.Bumper_callback)
+        self.sb = rospy.Subscriber("/mobile_base/sensors/core", SensorState, self.Bumper_callback)
+
+        self.laser_tb = rospy.Subscriber("/scan", LaserScan, self.Lasers_callback)
 
         self.left_bumper_fastsim = rospy.Publisher('/simu_fastsim/left_bumper', Bool, queue_size=10)
         self.right_bumper_fastsim = rospy.Publisher('/simu_fastsim/right_bumper', Bool, queue_size=10)
+        self.laser_fastsim = rospy.Publisher('/simu_fastsim/laser_scan', LaserScan, queue_size=10)
 
  
         # What function to call when you ctrl + c    
@@ -54,7 +61,7 @@ class TranslateFastsimTurtlebot():
             
             move_cmd.linear.x = (self.speed_left+self.speed_right)/2.0
             move_cmd.angular.z = (self.speed_right-self.speed_left)/0.5
-
+            print("speed: linear="+str(move_cmd.linear.x)+" angular="+str(move_cmd.angular.z))
             # publish the velocity
             self.cmd_vel.publish(move_cmd)
 
@@ -62,6 +69,9 @@ class TranslateFastsimTurtlebot():
             self.left_bumper_fastsim.publish(self.left_bumper)
             self.right_bumper_fastsim.publish(self.right_bumper)
  
+            # publish the laser
+            self.laser_fastsim.publish(self.lasers)
+
             # wait 
             r.sleep()
 
@@ -73,21 +83,38 @@ class TranslateFastsimTurtlebot():
   # sleep just makes sure TurtleBot receives the stop command prior to shutting down the script
         rospy.sleep(1)
                         
+
+    def Lasers_callback(self,data):
+        self.lasers=data
+        #rospy.loginfo(rospy.get_caller_id()+" Lasers %s"," ".join(map(str,lasers)))
+
     def Speed_left_callback(self,data):
         self.speed_left=float(data.data)
-        rospy.loginfo("Received speed left: "+str(self.speed_left))
+        print("Speed left: "+str(self.speed_left))
+	if (abs(self.speed_left)>speed_max):
+          rospy.loginfo("Left speed too fast: "+str(self.speed_left))
+	  self.speed_left=self.speed_left/abs(self.speed_left)*speed_max
+        #rospy.loginfo("Received speed left: "+str(self.speed_left))
 
     def Speed_right_callback(self,data):
         self.speed_right=float(data.data)
-        rospy.loginfo("Received speed right: "+str(self.speed_right))
+        print("Speed right: "+str(self.speed_right))
+	if (abs(self.speed_right)>speed_max):
+          rospy.loginfo("Right speed too fast: "+str(self.speed_right))
+	  self.speed_right=self.speed_right/abs(self.speed_right)*speed_max
 
     def Bumper_callback(self,data):
-        rospy.loginfo("Received bumper")
-        if (data.bumper==data.LEFT):
-            self.left_bumper=data.state==data.PRESSED
-        elif (data.bumper==data.RIGHT):
-            self.right_bumper=data.state==data.PRESSED
-
+        #rospy.loginfo("Received bumper")
+        if (data.bumper==4):
+            self.left_bumper=True
+        elif (data.bumper==1):
+            self.right_bumper=True
+        elif (data.bumper==2):
+            self.right_bumper=True
+            self.left_bumper=True
+	else:
+            self.right_bumper=False
+            self.left_bumper=False
 
 
 
